79559c3b523355e2dfa46e9b21f3c1b6dbc6c614
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2018-07-16 18:27:56 +0300

    feature(parcel): bundled everything to single executable

--- bin/dts-generator.js
+++ bin/dts-generator.js
@@ -1,76 +1,65 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "../index"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    var index_1 = require("../index");
-    function main(argv) {
-        var kwArgs = {
-            files: [],
-            sendMessage: console.log.bind(console)
-        };
-        for (var i = 0; i < argv.length; ++i) {
-            var arg = argv[i];
-            if (arg.charAt(0) === '-') {
-                var key = argv[i].replace(/^-+/, '');
-                var value = argv[i + 1];
-                ++i;
-                if (key === 'exclude') {
-                    if (!kwArgs.exclude) {
-                        kwArgs.exclude = [];
-                    }
-                    kwArgs.exclude.push(value);
+var index_1 = require("./util.js");
+function main(argv) {
+    var kwArgs = {
+        files: [],
+        sendMessage: console.log.bind(console)
+    };
+    for (var i = 0; i < argv.length; ++i) {
+        var arg = argv[i];
+        if (arg.charAt(0) === '-') {
+            var key = argv[i].replace(/^-+/, '');
+            var value = argv[i + 1];
+            ++i;
+            if (key === 'exclude') {
+                if (!kwArgs.exclude) {
+                    kwArgs.exclude = [];
                 }
-                else if (key === 'extern') {
-                    if (!kwArgs.externs) {
-                        kwArgs.externs = [];
-                    }
-                    kwArgs.externs.push(value);
+                kwArgs.exclude.push(value);
+            }
+            else if (key === 'extern') {
+                if (!kwArgs.externs) {
+                    kwArgs.externs = [];
                 }
-                else if (key === 'types') {
-                    if (!kwArgs.types) {
-                        kwArgs.types = [];
-                    }
-                    kwArgs.types.push(value);
+                kwArgs.externs.push(value);
+            }
+            else if (key === 'types') {
+                if (!kwArgs.types) {
+                    kwArgs.types = [];
                 }
-                else if (key === 'verbose') {
-                    kwArgs.verbose = true;
-                    /* decrement counter, because vebose does not take a value */
-                    --i;
-                }
-                else {
-                    kwArgs[key] = value;
-                }
+                kwArgs.types.push(value);
+            }
+            else if (key === 'verbose') {
+                kwArgs.verbose = true;
+                /* decrement counter, because vebose does not take a value */
+                --i;
             }
             else {
-                kwArgs.files.push(argv[i]);
+                kwArgs[key] = value;
             }
         }
-        ['out'].forEach(function (key) {
-            if (!kwArgs[key]) {
-                console.error("Missing required argument \"" + key + "\"");
-                process.exit(1);
-            }
-        });
-        if (!kwArgs.baseDir && !kwArgs.project) {
-            console.error("Missing required argument of \"baseDir\" or \"project\"");
-            process.exit(1);
+        else {
+            kwArgs.files.push(argv[i]);
         }
-        if (!kwArgs.project && kwArgs.files.length === 0) {
-            console.error('Missing files');
-            process.exit(1);
-        }
-        console.log('Starting');
-        return index_1.default(kwArgs).then(function () {
-            console.log('Done!');
-        });
     }
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.default = main;
-    ;
-});
+    ['out'].forEach(function (key) {
+        if (!kwArgs[key]) {
+            console.error("Missing required argument \"" + key + "\"");
+            process.exit(1);
+        }
+    });
+    if (!kwArgs.baseDir && !kwArgs.project) {
+        console.error("Missing required argument of \"baseDir\" or \"project\"");
+        process.exit(1);
+    }
+    if (!kwArgs.project && kwArgs.files.length === 0) {
+        console.error('Missing files');
+        process.exit(1);
+    }
+    console.log('Starting');
+    return index_1.default(kwArgs).then(function () {
+        console.log('Done!');
+    });
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = main;
+;
